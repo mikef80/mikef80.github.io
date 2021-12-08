2c8ed7001611c0c411f1776d49cf3aa0bfd080b9
Mike <mike@mike-francis.org>
2021-12-08 21:45:46 +0000

    added some HTML to start playing with JS integration. Also added a couple of tests in JS to display on page.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const colorChange = (button) => {
-    let 
-}
+// THEME CHANGE SWITCH
+const setTheme = theme => document.documentElement.className = theme;
+
+// TIME SENSITIVE WELCOME MESSAGE
+//to do...
+
+let test = "this is a test";
+document.getElementById("welcome").innerHTML = "<p>Hello!</p>";
+document.getElementById("welcome1").innerHTML = test;
+document.getElementById("welcome2").innerHTML = `The test variable says "${test}".`;

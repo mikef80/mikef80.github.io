d2e383f701e1608cb8db2ce8c9f42400870df974
Mike <mike@mike-francis.org>
2021-12-09 12:29:16 +0000

    added JS functionality to display time and change theme colours automatically dependent on time of day.

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 const setTheme = theme => document.documentElement.className = theme;
 
 // TIME SENSITIVE WELCOME MESSAGE
-//to do...
+var today = new Date();
+var time = new Date().toLocaleTimeString();
+var hour = today.getHours();
+var greeting = document.getElementById("time-greeting");
+
+if (hour < 12) {
+    greeting.innerHTML = `Good morning! It's ${time}`;
+} else if (hour >= 12 && hour < 18) {
+    greeting.innerHTML = `Good Afternoon! It's ${time}`;
+} else {
+    greeting.innerHTML = `Good Evening! It's ${time}`;
+    setTheme('dark');
+}
+
+// TEST JS CODE
 
 let test = "this is a test";
 document.getElementById("welcome").innerHTML = "<p>Hello!</p>";

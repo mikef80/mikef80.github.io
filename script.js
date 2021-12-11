bf7df051d98f9e6cc23e06bd20e25de0b2ebb1a2
Mike <mike@mike-francis.org>
2021-12-11 12:14:29 +0000

    pushing realtime clock updates

--- script.js
+++ script.js
@@ -2,23 +2,42 @@
 const setTheme = theme => document.documentElement.className = theme;
 
 // TIME SENSITIVE WELCOME MESSAGE
+
 var today = new Date();
 var time = new Date().toLocaleTimeString();
-var hour = today.getHours();
-var greeting = document.getElementById("time-greeting");
+var greeting = document.getElementById('time-greeting');
 
-if (hour < 12) {
+if (time < '12:00:00') {
     greeting.innerHTML = `Good morning! It's ${time}`;
-} else if (hour >= 12 && hour < 18) {
+} else if (time >= '12:00:00' && time < '18:00:00') {
     greeting.innerHTML = `Good Afternoon! It's ${time}`;
 } else {
     greeting.innerHTML = `Good Evening! It's ${time}`;
     setTheme('dark');
 }
 
+// CLOCK FUNCTION
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+//   document.getElementById('startTime').innerHTML =  h + ":" + m + ":" + s;
+  document.getElementById('startTime').innerHTML =  `${h}:${m}:${s}`;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
 // TEST JS CODE
 
-let test = "this is a test";
-document.getElementById("welcome").innerHTML = "<p>Hello!</p>";
-document.getElementById("welcome1").innerHTML = test;
-document.getElementById("welcome2").innerHTML = `The test variable says "${test}".`;
+var test = "this is a test";
+document.getElementById('welcome').innerHTML = '<p>Hello!</p>';
+document.getElementById('welcome1').innerHTML = test;
+document.getElementById('welcome2').innerHTML = `The test variable says "${test}".`;

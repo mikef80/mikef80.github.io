9fee2ac67f313ec2c79f022da013e7197fc6ab66
Mike <mike@mike-francis.org>
2021-12-29 19:28:50 +0000

    js updates

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const setTheme = theme => document.documentElement.className = theme;
 
 // TIME SENSITIVE WELCOME MESSAGE
 
-var today = new Date();
+/* var today = new Date();
 var time = new Date().toLocaleTimeString();
 var greeting = document.getElementById('time-greeting');
 
@@ -14,18 +14,17 @@ if (time < '12:00:00') {
 } else {
     greeting.innerHTML = `Good Evening! It's ${time}`;
     setTheme('dark');
-}
+} */
 
 // CLOCK FUNCTION
 
-function startTime() {
+/* function startTime() {
   const today = new Date();
   let h = today.getHours();
   let m = today.getMinutes();
   let s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
-//   document.getElementById('startTime').innerHTML =  h + ":" + m + ":" + s;
   document.getElementById('startTime').innerHTML =  `${h}:${m}:${s}`;
   setTimeout(startTime, 1000);
 }
@@ -33,36 +32,15 @@ function startTime() {
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
-}
-
-// TEST JS CODE
-
-/* var test = "this is a test";
-document.getElementById('welcome').innerHTML = 'Hello!';
-document.getElementById('welcome1').innerHTML = test;
-document.getElementById('welcome2').innerHTML = `The test variable says "${test}".`; */
-
-// GALLERY SLIDER
-
-let slider = document.getElementById("gallerySize");
-let output = document.getElementById("output");
-let test = document.getElementsByClassName("pics")
-
-output.innerHTML = `${slider.value}px`;
-
-gallerySize.oninput = function () {
-  for (let i = 0; i < pics.length; i++) {
-    pics[i].style.width =  gallerySize.value;
-  }
-}
+} */
 
 // GET CURRENT YEAR
 
-const currentYear = () => {
-  const year = new Date().getFullYear();
-  console.log(year);
+// let yearNow;
 
-  const d = new Date();
-  let year2 = d.getFullYear();
-  console.log(year2)
+const currentYear = () => {
+  let yearNow = new Date().getFullYear();
+  document.getElementById('copyright').innerHTML = `<ul><li>Copyright Mike Francis ${yearNow}</li></ul>`;
 ;}
+
+currentYear();

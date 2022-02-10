00ece250be9202e51bd57f4c6ebf2fd7a13786b1
Mike Francis <mfrancis@clever-touch.com>
2022-02-10 16:58:18 +0000

    updated app.js with api learning from https://www.youtube.com/watch?v=5QlE6o-iYcE

--- Version 2/app.js
+++ Version 2/app.js
@@ -1,89 +1,31 @@
-const body = document.querySelector('.github');
-const myRequest = 'https://api.github.com/users/mikef80/repos';
-const langTest = 'https://api.github.com/repos/mikef80/TechncialTest/languages';
-const langTestURL = 'https://api.github.com/repos/mikef80/';
-const langTestSuffix = '/languages';
+const portfolio = document.querySelector('.github');
+const click = document.querySelector('#test');
 
-body.innerHTML = 'Hello, World!';
+const getRepos = async () => {
+  const url = 'https://api.github.com/search/repositories?q=stars:>200000';
+  const response = await fetch(url);
+  const result = await response.json();
 
-const test = async () => {
-  await fetch(myRequest)
-    .then(response => response.json())
-    .then(json => console.log(json))
-    /* .then(array => {
-      array.forEach(element => {
-          // console.log(element);
-          let name = element.name.slice(0,2);
-          // console.log(name)
-          if (name === 'CA') {
-            console.log(element.name);
-          }
-      });
-    }) */
+  result.items.forEach(i => {
+
+
+    const anchor = document.createElement('a');
+    anchor.href = i.html_url;
+    anchor.textContent = i.full_name;
+    portfolio.appendChild(anchor);
+    portfolio.appendChild(document.createElement('br'));
+
     
+  });
 }
 
-
-// LANGUAGE BREAKDOWN PER REPO
-const lang = async () => {
-  await fetch(langTest)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      let sum = 0;
-      let cssTot = json.CSS;
-      let htmlTot = json.HTML;
-      let jsTot = json.JavaScript;
-
-      sum = cssTot + htmlTot + jsTot;
-
-      console.log(sum);
-
-      cssTot = cssTot / sum;
-      htmlTot = htmlTot / sum;
-      jsTot = jsTot / sum;
-
-      console.log(cssTot);
-      console.log(htmlTot);
-      console.log(jsTot);
-      console.log(jsTot + cssTot + htmlTot);
-
-
-    })    
+const clear = () => {
+  while(portfolio.firstChild) {
+    portfolio.removeChild(portfolio.firstChild);
+  }
 }
 
-
-
-// LANGUAGE BREAKDOWN PER REPO TEST
-const lang2 = async () => {
-  await fetch(langTest)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      let sum = 0;
-      let cssTot = json.CSS;
-      let htmlTot = json.HTML;
-      let jsTot = json.JavaScript;
-
-      sum = cssTot + htmlTot + jsTot;
-
-      console.log(sum);
-
-      cssTot = cssTot / sum;
-      htmlTot = htmlTot / sum;
-      jsTot = jsTot / sum;
-
-      console.log(cssTot);
-      console.log(htmlTot);
-      console.log(jsTot);
-      console.log(jsTot + cssTot + htmlTot);
-
-
-    })    
-}
-
-
-
-
-test();
-// lang();
+click.addEventListener('click', () => {
+  clear();
+  getRepos();
+})

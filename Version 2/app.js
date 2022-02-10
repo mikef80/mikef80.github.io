05972e76244b4f6755daf8b81a8ded00989dff56
Mike Francis <mfrancis@clever-touch.com>
2022-02-10 17:36:33 +0000

    currently at 28:40

--- Version 2/app.js
+++ Version 2/app.js
@@ -1,7 +1,9 @@
 const portfolio = document.querySelector('.github');
-const click = document.querySelector('#test');
+const click = document.querySelector('#repos');
+const issues = document.querySelector('#issues');
 
 const getRepos = async () => {
+  clear();
   const url = 'https://api.github.com/search/repositories?q=stars:>200000';
   const response = await fetch(url);
   const result = await response.json();
@@ -19,13 +21,34 @@ const getRepos = async () => {
   });
 }
 
+
+const getIssues = async () => {
+  clear();
+  const url = 'https://api.github.com/search/issues?q=author:mikef80';
+  const response = await fetch(url);
+  const result = await response.json();
+
+  result.items.forEach(i => {
+
+
+    const anchor = document.createElement('a');
+    anchor.href = i.html_url;
+    anchor.textContent = i.title;
+    portfolio.appendChild(anchor);
+    portfolio.appendChild(document.createElement('br'));
+
+    
+  });
+}
+
+
+
+
 const clear = () => {
   while(portfolio.firstChild) {
     portfolio.removeChild(portfolio.firstChild);
   }
 }
 
-click.addEventListener('click', () => {
-  clear();
-  getRepos();
-})
+click.addEventListener('click', getRepos)
+issues.addEventListener('click', getIssues)

e4ac5d43f47578aef714cec5eccda4df460d2cd5
Mike <mike@mike-francis.org>
2022-02-10 21:58:12 +0000

    trying to work out apis

--- Version 2/app.js
+++ Version 2/app.js
@@ -1,13 +1,15 @@
 const portfolio = document.querySelector('.github');
 const click = document.querySelector('#repos');
 const issues = document.querySelector('#issues');
+const mclick = document.querySelector('#mrepos');
+const display = document.querySelector('#display');
 
 const getRepos = async () => {
   clear();
   const url = 'https://api.github.com/search/repositories?q=stars:>200000';
   const response = await fetch(url);
   const result = await response.json();
-
+  display.innerHTML = 'repos';
   result.items.forEach(i => {
 
 
@@ -21,13 +23,36 @@ const getRepos = async () => {
   });
 }
 
+const getMrepos = async () => {
+  clear();
+  const url = 'https://api.github.com/users/mikef80/repos';
+  // const url = 'https://api.github.com/search/repositories?q=owner[login]:mikef80';
+  // const url = 'https://api.github.com/search/repositories?q=name:';
+  console.log(url);
+  const response = await fetch(url);
+  const result = await response.json();
+  // console.log(result);
+  display.innerHTML = 'Mrepos';
+  result.forEach(i => {
+
+    console.log(i);
+    const anchor = document.createElement('a');
+    anchor.href = i.html_url;
+    anchor.textContent = i.name;
+    portfolio.appendChild(anchor);
+    portfolio.appendChild(document.createElement('br'));
+   
+
+    
+  });
+}
 
 const getIssues = async () => {
   clear();
-  const url = 'https://api.github.com/search/issues?q=author:mikef80';
+  const url = 'https://api.github.com/search/issues?q=author:mikef80 type:issue';
   const response = await fetch(url);
   const result = await response.json();
-
+  display.innerHTML = 'issues';
   result.items.forEach(i => {
 
 
@@ -51,4 +76,5 @@ const clear = () => {
 }
 
 click.addEventListener('click', getRepos)
+mclick.addEventListener('click', getMrepos)
 issues.addEventListener('click', getIssues)

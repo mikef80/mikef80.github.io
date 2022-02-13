49fee0520839aa6dd42592bfc7897ae3c3072094
Mike <mike@mike-francis.org>
2022-02-13 19:36:45 +0000

    reworked so that api is doing the sorting

--- Version 2/app3.js
+++ Version 2/app3.js
@@ -1,17 +1,28 @@
 // ASYNC AWAIT BOILERPLATE
 
+const insertChild = (input) => {
+    input.items.forEach(element => {
+        console.log(element.name);
+        
+    });
+    
+}
+
 const getRepos = async () => {
     // const url = 'https://api.github.com/users/mikef80/repos';
-    const url = 'https://api.github.com/search/repositories?q=user:mikef80';
+    const repoName = 'CA-';
+    const url = `https://api.github.com/search/repositories?q=user:mikef80+CA+in:name+sort:updated`;
 
     try {
         const response = await fetch(url);
         if (response.ok) {
-            const jsonResponse = response.json();
+            const jsonResponse = await response.json();
             // Code to execute with jsonResponse
-            console.log(jsonResponse);
+            // console.log(jsonResponse); //USE TO CHECK RAW JSON RESPONSE
+
+            insertChild(jsonResponse);
+
         }
-        throw new Error('Request Failed!');
     } catch (error) {
         console.log(error);
     }

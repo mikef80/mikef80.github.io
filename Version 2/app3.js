0c285f67594b99290f3d60b4e4a4a9b903ebef74
Mike <mike@mike-francis.org>
2022-02-14 14:22:03 +0000

    many async await statements!

--- Version 2/app3.js
+++ Version 2/app3.js
@@ -1,21 +1,70 @@
 // ASYNC AWAIT BOILERPLATE
 
-const buildHTML = () => {
-    let output = [];
+// BUILD LANGUAGES HTML FOR INSERTION INTO buildHTML FUNCTION
+const buildLangs = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            let output = [];
+            
+            const array = Object.entries(jsonResponse);
+            output.push(`<p>${array}</p>`)
+            console.log(output.join(''));
+            return output.join('');
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-const insertChild = (input) => {
-    input.items.forEach(element => {
-        console.log(element.name);
-        
-    });
+// BUILD HTML FOR INDIVIDUAL PORTFOLIO CHILDREN
+// NEED TO INCLUDE LANGUAGES YET
+const buildHTML = async (input) => {
+    let output = [];
+    console.log(input);
     
+    let url = input.html_url;
+    let name = input.name;
+    let langsURL = input.languages_url;
+    let options = { /* weekday: 'long',  */year: 'numeric', month: 'long', day: 'numeric' };
+    let updated = new Date(input.updated_at).toLocaleString('en-GB',options);
+    const div = document.createElement('div');
+    div.classList.add('col-xl-3', 'col-md-4', 'col-sm-6', 'col-xs-9','portItem', 'p-3', 'rounded');
+
+    let langs = await buildLangs(langsURL);
+    
+    // let breakdown = langsBreakdown();
+
+    output.push(`<a href="${url}" target="_blank">
+                    <h4>${name}</h4>
+                    <h6>Last updated: ${updated}</h6>
+                    <p>${langs}</p>
+                    
+                </a>`);
+
+    div.innerHTML = output.join('');
+    // console.log(div);
+
+    return div;
+}
+
+// INSERT CHILD INTO PORTFOLIO
+const insertChild = (input) => {
+    const portItems = document.querySelector('.portItems');
+
+    input.items.forEach(async element => {
+        let output = await buildHTML(element);
+        // console.log(output);
+        portItems.appendChild(output);
+    });
 }
 
 const getRepos = async () => {
     // const url = 'https://api.github.com/users/mikef80/repos';
     const repoName = 'CA-';
-    const url = `https://api.github.com/search/repositories?q=user:mikef80+CA+in:name+sort:updated`;
+    const url = `https://api.github.com/search/repositories?q=user:mikef80+CAR+in:name+sort:updated`;
 
     try {
         const response = await fetch(url);

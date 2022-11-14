1c7b5f8d798b02d896129e39f58dd3e98a515f85
Mike <mike@mike-francis.org>
2022-11-14 22:13:16 +0000

    started bootstrap portfolio site

--- react-bootstrap/src/assets/Header/Header.jsx
+++ react-bootstrap/src/assets/Header/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-      <header className='display-1'>
-          Header
+      <header>
+          <div className='display-4'>Mike Francis</div>
+          <div className='lead'>Developer - /dɪˈvel.ə.pər/</div>
+          <div className='small'>a person or company that creates new products, especially computer products such as software</div>
       </header>
   )
 }

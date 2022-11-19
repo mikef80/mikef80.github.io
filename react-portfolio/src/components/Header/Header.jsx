11ecbc3ca128e9b6fac2429e2d5a5c6191d143dc
Mike <mike@mike-francis.org>
2022-11-19 20:28:45 +0000

    Updated fonts

--- react-portfolio/src/components/Header/Header.jsx
+++ react-portfolio/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import styles from './Header.module.css';
+
 const Header = () => {
   return (
-    <div className="flex flex-col sticky top-0 left-0 w-full bg-white">
-      <h1 className="text-5xl px-4 pt-4 pb-2 flex align-middle justify-center">
+    <header className="flex flex-col sticky top-0 left-0 w-full bg-white">
+      <h1 className="text-5xl px-4 pt-4 pb-2 flex align-middle justify-center font-semibold">
         Mike Francis
       </h1>
       <h2 className="text-3xl px-4 flex align-middle justify-center">
@@ -10,7 +12,7 @@ const Header = () => {
       <h3 className="text-1 px-4 pb-4 flex align-middle justify-center">
         (/dɪˈvɛləpə/)
       </h3>
-    </div>
+    </header>
   );
 };
 

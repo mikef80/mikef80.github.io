ca7f9269c9a1fc0b981327e4da3666db0529861b
Mike <mike@mike-francis.org>
2022-11-19 21:11:01 +0000

    created ProjectCard component to be populated by API call to GitHub. Currently filled with dummy data

--- react-portfolio/src/components/Projects/ProjectCard/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const ProjectCard = props => {
-    return <div className="border-2 rounded-md">
-        <h4>{props.title}</h4>
-        <img src={props.img} alt="" />
-        <p>{props.text}</p>
-  </div>;
+const ProjectCard = (props) => {
+  return (
+    <div className="border-2 rounded-md p-3 flex flex-col">
+      <h4>{props.title}</h4>
+      <img src={props.img} alt="" />
+      <p>{props.text}</p>
+    </div>
+  );
 };
 
 export default ProjectCard;
--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -12,41 +12,31 @@ import "swiper/css/scrollbar";
 const Projects = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      {/* <ProjectCard /> */}
-      <div>
-        <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={10}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          <SwiperSlide>
-            <ProjectCard
-              title="Project"
-              img="https://via.placeholder.com/400x200"
-              text="This is the project description"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ProjectCard
-              title="Project2"
-              img="https://via.placeholder.com/400x200"
-              text="This is another project description"
-            />
-          </SwiperSlide>
-          {/* <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide> */}
-        </Swiper>
-        Projects
-        <button onClick={() => navigate("/")}>Go Home</button>
-        <button onClick={() => navigate("/contact")}>Go To Contact</button>
-      </div>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={10}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={ (swiper) => console.log(swiper) }
+      
+    >
+      <SwiperSlide>
+        <ProjectCard
+          title="Project"
+          img="https://via.placeholder.com/400x200"
+          text="This is the project description"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <ProjectCard
+          title="Project2"
+          img="https://via.placeholder.com/400x200"
+          text="This is another project description"
+        />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 

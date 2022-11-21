3784afec76267d972a2984706537a5729f0b1d02
Mike <mike@mike-francis.org>
2022-11-21 14:05:02 +0000

    added custom breakpoint to config file to adjust header visibility in mobile portrait mode

--- react-portfolio/src/components/Header/Header.jsx
+++ react-portfolio/src/components/Header/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
       <h1 className="text-5xl px-4 pt-4 pb-2 flex align-middle justify-center font-semibold">
         Mike Francis
       </h1>
-      <h2 className="text-3xl px-4 flex align-middle justify-center">
+      <h2 className="text-3xl px-4 flex align-middle justify-center mob-landscape:hidden">
         Developer
       </h2>
-      <h3 className="text-1 px-4 pb-4 flex align-middle justify-center">
+      <h3 className="text-1 px-4 pb-4 flex align-middle justify-center mob-landscape:hidden">
         (/dɪˈvɛləpə/)
       </h3>
       <NavBar />
--- react-portfolio/tailwind.config.cjs
+++ react-portfolio/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "mob-landscape": { raw: "(orientation: landscape)" },
+      },
+    },
   },
   plugins: [],
 };

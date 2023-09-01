5549f2d4b7a35a1202298b2d51f1c7d945df8ec4
Mike <mike@mike-francis.org>
2023-09-01 21:13:32 +0100

    picking up again

--- src/components/PortfolioItem/PortfolioItem.tsx
+++ src/components/PortfolioItem/PortfolioItem.tsx
@@ -1,15 +1,18 @@
-const PortfolioItem = (item) => {
-  console.log('====================================');
+const PortfolioItem = (item: any) => {
+  /* console.log('====================================');
   console.log(item.item);
-  console.log('====================================');
+  console.log('===================================='); */
 
-  const { name } = item.item;
+  const { name, languages_url, html_url, updated_at } = item.item;
+  const updatedDate = new Date(updated_at).toLocaleDateString();
+  console.log(languages_url);
+  
 
   return (
     <div className="border-[1px] border-black rounded-md p-4 lg:w-1/4">
       <a href="https://github.com/mikef80/lemon-and-jinja" target="_blank" className="flex flex-col text-center">
         <h4 className="text-xl font-semibold">{name}</h4>
-        <h6>Last updated: 28 June 2023</h6>
+        <h6>Last updated: {updatedDate}</h6>
         <p>TypeScript + HTML + JavaScript + CSS</p>
         <p>TypeScript: 75% | HTML: 23% | JavaScript: 1% | CSS: 0%</p>           
       </a>
